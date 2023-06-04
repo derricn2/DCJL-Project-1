50e1e7c5fa0738bf00b7d60db37c4760fbd44ee4
derricn2 <derricn2@gmail.com>
2023-06-03 20:51:31 -0700

    worked on JavaScript, integrating Tailwind CSS to style the movie cards and movie list

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,9 +38,10 @@ function searchMovies(searchTerm) {
 function createMovieCard(movie) {
 
     var card = document.createElement('div');
-    card.classList.add('movie-card', 'w-1/5', 'bg-[#27374D]', 'text-[#9DB2BF]', 'p-4', 'box-border', 'border', 'border-black', 'rounded-md', 'text-center');
+    card.classList.add('movie-card', 'w-1/5', 'bg-[#27374D]', 'text-[#9DB2BF]', 'p-4', 'box-border', 'border', 'border-black', 'rounded-md', 'text-center', 'relative', 'mr-2');
 
     var poster = document.createElement('img');
+    poster.classList.add('mx-auto', 'relative', 'w-full', 'h-auto');
     poster.src = movie.Poster;
     poster.alt = movie.Title;
   
@@ -49,11 +50,13 @@ function createMovieCard(movie) {
     title.textContent = movie.Title;
   
     var year = document.createElement('p');
+    year.classList.add('mb-3');
     year.textContent = `${movie.Year}`;
 
     var addButton = document.createElement('button');
     addButton.textContent = 'Add to List';
-    addButton.classList.add('btn-add', 'px-4', 'py-2', 'bg-[#27374D]','rounded-md', 'hover:bg-[#DDE6ED]');
+    addButton.classList.add('btn-add', 'px-2', 'py-2', 'bg-[#9DB2BF]', 'rounded-md', 'hover:bg-[#DDE6ED]', 'box-border', 'border', 'border-black', 'bottom-2', 'left-1/2', 'transform', '-translate-x-1/2', 'translate-y-1/2', 'relative', 'bottom-0', 'w-full');
+    addButton.style.color = '#27374D';
 
     addButton.addEventListener('click', function () {
         addMovieToList(movie.Title);
@@ -81,6 +84,8 @@ function addMovieToList(title) {
     }
     var listItem = document.createElement('li');
     listItem.textContent = title;
+    listItem.classList.add('hover:bg-[#DDE6ED]', 'hover:cursor-pointer')
+    listItem.style.color = '#27374D';
     
     selectedList.appendChild(listItem);
   }
